9b484bd6120531d2b57dd89221db8fa874841377
Guillermo <memocargo@hotmail.com>
2022-08-11 20:50:13 -0500

    Views y templates agregadas y funcionando

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 
 router.get('/users/signin', (req, res) => {
-  res.send('ingresando a la app')
+  res.render('users/signin')
 })
 
 router.get('/users/signup', (req, res) => {
-  res.send('formulario de autenticacion')
+  res.render('users/signup')
 })
 
 module.exports = router
