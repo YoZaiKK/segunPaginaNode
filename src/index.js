14627941f3eacfc1838e2d777356a45c0844444c
Guillermo <memocargo@hotmail.com>
2022-08-19 22:16:43 -0500

    sign in creado y funcionando

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const flash = require('connect-flash')
+const passport = require('passport')
 
 
 // Initialize
 const app = express();
 require('./database')
+require('./config/passport')
 
 // Settings
 console.log(exphbs);
@@ -30,6 +32,8 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash())
 
 
@@ -38,6 +42,7 @@ app.use((req, res, next) => {
 
   res.locals.success_msg = req.flash('success_msg')
   res.locals.error_msg = req.flash('error_msg')
+  res.locals.error = req.flash('error')
   next();
 })
 
